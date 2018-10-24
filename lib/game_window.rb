9b887eeb8297a979162c171d8a860fe407409155
Lean <lndr.figueredo@gmail.com>
2018-10-24 15:28:47 -0300

    Moving Player in Game Window

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'game_window/player'
 
 class GameWindow < Gosu::Window
 
@@ -6,12 +7,29 @@ class GameWindow < Gosu::Window
     super 640, 400, false
     self.caption = 'Gosu Tutorial Game'.freeze
     @background_image = Gosu::Image.new(self, 'media/space.png', true)
+    @player = Player.new(self)
+    @player.warp(320,240)
   end
 
   def update
+    if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
+      @player.turn_left
+    end
+    if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
+      @player.turn_right
+    end
+    if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+      @player.accelerate
+    end
+    @player.move
   end
 
   def draw
+    @player.draw
     @background_image.draw(0,0,0)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
   end
 end
--- lib/game_window/player.rb
+++ lib/game_window/player.rb
@@ -1,7 +1,7 @@
 class Player
   def initialize(window)
-    @image = Gosu::image.new(window, 'media/starfighter.bmp', false)
-    @x = @y = @vel_x = @val_y = @angle = 0.0
+    @image = Gosu::Image.new(window, "media/test.bmp", false)
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
     @score = 0
   end
 
@@ -10,17 +10,29 @@ class Player
   end
 
   def turn_left
+    @angle -= 4.5
   end
 
   def turn_right
+    @angle += 4.5
   end
 
   def accelerate
+    @vel_x += Gosu::offset_x(@angle, 0.5)
+    @vel_y += Gosu::offset_y(@angle, 0.5)
   end
 
   def move
+    @x += @vel_x
+    @y += @vel_y
+    @x %= 640
+    @y %= 480
+
+    @vel_x *= 0.95
+    @vel_y *= 0.95
   end
 
   def draw
+    @image.draw_rot(@x, @y, 1, @angle)
   end
 end
